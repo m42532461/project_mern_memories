50495a2c12f13ad41894e79a6184bb9a295d858d
m42532461 <m42532461@gmail.com>
2022-08-15 15:10:12 +0800

    add search, pagination at frontend

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,28 +1,109 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Grow,
+  Grid,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-import { getPosts } from '../../actions/posts';
-import Posts from '../Posts/Posts';
-import Form from '../Form/Form';
+import { getPosts } from "../../actions/posts";
+import Pagination from "../Pagination";
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+
+import ChipInput from "material-ui-chip-input";
+import { useLocation, useHistory } from "react-router-dom";
+import useStyles from "./styles";
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const Home = () => {
+  const classes = useStyles();
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
-
+  const query = useQuery();
+  const history = useHistory();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
 
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
+  const handleKeyPress = (event) => {
+    if (event.keyCode === 13) {
+      // event.keyCode === 13 means press enter
+      // Search post
+      searchPost();
+    }
+  };
+  const handleAdd = (tag) => setTags([...tags, tag]);
+  const handleDelete = (tagToDelete) =>
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+  const searchPost = () => {
+    if (search.trim()) {
+      // dispatch -> fetchPosts
+    } else {
+      history.push("/");
+    }
+  };
   return (
     <Grow in>
-      <Container>
-        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-          <Grid item xs={12} sm={7}>
+      <Container maxWidth="xl">
+        <Grid
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+          className={classes.gridContainer}
+        >
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              postion="static"
+              color="inherit"
+            >
+              <TextField
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                fullWidth
+                onKeyPress={handleKeyPress}
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+              <ChipInput
+                style={{ margin: "10px 0" }}
+                value={tags}
+                onAdd={handleAdd}
+                onDelete={handleDelete}
+                label="Search Tags"
+                variant="outlined"
+              />
+              <Button
+                onClick={searchPost}
+                className={classes.searchButton}
+                color="primary"
+                variant="contained"
+              >
+                Search
+              </Button>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper elevation={6}>
+              <Pagination />
+            </Paper>
           </Grid>
         </Grid>
       </Container>
